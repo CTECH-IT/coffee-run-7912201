5bf3fbcb02126648b02b469870129e9e839b19ca
Caleb Pardeik <7912201@isd535.org>
2022-03-10 11:01:50 -0600

    AD truck order and deliver

--- scripts/truck.js
+++ scripts/truck.js
@@ -3,8 +3,19 @@
 
     let App = window.App || {};
 
-    function Truck() {
+    function Truck(truckID, db) {
+        this.truckID = truckID;
+        this.db = db;
+    }
+    
+    Truck.prototype.createOrder = function(order) {
+        console.log("Adding order for " + order.emailAddress);
+        this.db.add(order.emailAddress, order)
+    }
 
+    Truck.prototype.deliverOrder = function(customerID) {
+        console.log("delivering order for " + customerID);
+        this.db.remove(customerID)
     }
 
     App.Truck = Truck;
