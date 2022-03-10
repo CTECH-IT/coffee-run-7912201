1c5c72cf1ff79ee87030d7dd7ed2602c2a12c7ab
Caleb Pardeik <7912201@isd535.org>
2022-03-10 11:27:55 -0600

    AD main.js AD printorders

--- scripts/truck.js
+++ scripts/truck.js
@@ -18,6 +18,21 @@
         this.db.remove(customerID)
     }
 
+    Truck.prototype.printOrders = function() {
+
+        //get all the email adresses (keys)
+        let customerIDArray = Object.keys(this.db.getAll());
+
+        console.log("Truck #" + this.truckID + " has pending orders:");
+
+        //go through the list of emails I just created and get the order
+        customerIDArray.forEach(function(id) {
+            console.log(this.db.get(id));
+        }.bind(this));
+
+
+    }
+
     App.Truck = Truck;
     window.App = App;
 
